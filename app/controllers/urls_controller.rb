2c7cb66debf0f93b8e1c6f00a3efb508a8064a81
Matthieu Rakotojaona <matthieu.rakotojaona@gmail.com>
2016-07-27 19:54:39 +0200

    Remove default configuration that isn't used

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,5 +1,5 @@
 class UrlsController < ApplicationController
-  before_action :set_url, only: [:show, :edit, :update, :destroy]
+  before_action :set_url, only: [:show]
 
   # GET /urls
   # GET /urls.json
