d5b561eb6b634f0a2aed683e857ca7a746cafd9a
Matthieu Rakotojaona <matthieu.rakotojaona@gmail.com>
2016-07-28 00:08:40 +0200

    Add :create action for reports controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :reports, only: [:show, :index, :new, :destroy]
+  resources :reports, only: [:show, :index, :new, :destroy, :create]
   resources :keywords, only: [:show, :index]
   get '/keywords/:id/cache', controller: "keywords", action: "cache"
 
